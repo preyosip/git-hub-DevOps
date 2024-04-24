73f8dd330ee0a7a104c8b57cd3e5025fc1dd67cb
preyosip <104801647+preyosip@users.noreply.github.com>
2024-04-24 14:41:45 +0100

    Update Pythn.py
    program  code using lambda function to do cube of a list of numbers.

--- Pythn.py
+++ Pythn.py
@@ -1,1 +1,10 @@
+def my_map(my_func , my_item):
+   result = []
+   for item in my_item:
+      new_item = my_func(item)
+      result.append(my_item)
+   return result 
+num = [2,3,4,5,6,7]
 
+cubed = my_map(lambda x : x**3 , num)
+print(cubed)
